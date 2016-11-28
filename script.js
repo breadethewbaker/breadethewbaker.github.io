23120959767aa4e1dcded1f3e5b09494323194d2
Brett Baker <brettmatt8@hotmail.com>
2016-11-28 15:20:19 -0500

    starting to add sudoku

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
-var _divClick = 0;
-
-$('#hl').click(function(){
-  if (_divClick === 0){
-    $("#hl").html("Carmen Sandiego");
-    $('.acts').remove()
-    $('#ul').append("<ul class='acts'><li id='add'>Hiding</li></ul>");
-    _divClick = 1;
-  }
-  else{
-    $("#hl").html("<a>Brett Baker</a>");
-    $("#add").remove();
-    $('.acts').append("<li>Running</li>\n<li>Riding my bicycle</li>\n<li>Playing tennis</li>\n<li>Reading</li>\n<li>Baking</li>");
-    _divClick = 0;
-  }
-  $('.title').toggleClass("cartitle");
-  $('.lbar').toggleClass("carlbar");
+$(document).ready(function(){
+  var _divClick = 0;
+  $('#hclick').click(function(){
+    if (_divClick === 0){
+      $("#hl").html("<li id='hl'><a href='index.html'>Carmen Sandiego</a></li>");
+      $('.acts').remove()
+      $('#ul').append("<ul class='acts'><li id='add'>Hiding</li></ul>");
+      _divClick = 1;
+    }
+    else{
+      $("#hl").html("<li id='hl'><a href='index.html'>Brett Baker</a></li>");
+      $("#add").remove();
+      $('.acts').append("<li>Running</li>\n<li>Riding my bicycle</li>\n<li>Playing tennis</li>\n<li>Reading</li>\n<li>Baking</li>");
+      _divClick = 0;
+    }
+    $('.title').toggleClass("cartitle");
+    $('.lbar').toggleClass("carlbar");
+  });
+  $('#sudoku').append
 });
